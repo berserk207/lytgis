f73519d60220b5cffa6555820b7c27e14861df08
Clark <clark56@gmail.com>
2015-05-26 09:45:46 +0800

    corrected syntax error

--- docs/conf.py
+++ docs/conf.py
@@ -12,13 +12,16 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+
 import sys
 import os
 import shlex
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]	
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
